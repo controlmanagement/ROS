3506360fa9ac03b9f375c04d22844e186c449934
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2017-09-20 19:04:13 +0900

    未完成
    antenna 駆動と weather 情報の取得は可能

--- ROS_status.py
+++ ROS_status.py
@@ -3,6 +3,7 @@
 import time
 import rospy
 from ros_start.msg import Status_antenna_msg
+from ros_start.msg import Weather_msg
 
 class status_main(object):
     param1 = {
@@ -14,15 +15,29 @@ class status_main(object):
         "current_el":0,
         "emergency" :0
         }
-    param2 = {"in_temp":,
-
+    param2 = {"in_temp":0,
+              "out_temp":0,
+              "in_humi":0,         
+              "out_humi":0,
+              "wind_sp":0,
+              "wind_dir":0,
+              "press":0,
+              "rain":0,
+              "cabin_temp1":0,
+              "cabin_temp2":0,
+              "dome_temp1":0,
+              "dome_temp2":0,
+              "gen_temp1":0,
+              "gen_temp2":0,
+              }
 
 
     def __init__(self):
         pass
 
     def status_check(self):
-        rospy.loginfo(self.parameters)
+        #rospy.loginfo(self.param1)
+        rospy.loginfo(self.param2)
 
     def callback1(self, req):
         self.param1["limit_az"] = req.limit_az
@@ -35,6 +50,21 @@ class status_main(object):
         self.status_check()
 
     def callback2(self, req):
+        self.param2["in_temp"] = req.in_temp
+        self.param2["out_temp"]= req.out_temp
+        self.param2["in_humi"]= req.in_humi
+        self.param2["out_humi"]= req.out_humi
+        self.param2["wind_sp"]= req.wind_sp
+        self.param2["wind_dir"]= req.wind_dir
+        self.param2["press"]= req.press
+        self.param2["rain"]= req.rain
+        self.param2["cabin_temp1"]= req.cabin_temp1
+        self.param2["cabin_temp2"]= req.cabin_temp2
+        self.param2["dome_temp1"]= req.dome_temp1
+        self.param2["dome_temp2"]= req.dome_temp2
+        self.param2["gen_temp1"]= req.gen_temp1
+        self.param2["gen_temp2"]= req.gen_temp2
+        self.status_check()
         pass
 
 
@@ -45,6 +75,6 @@ if __name__ == '__main__':
     rospy.init_node('Status')
     sub1 = rospy.Subscriber('status_antenna', Status_antenna_msg, st.callback1)
     time.sleep(0.01)
-    sub2 = rospy.Subscriber('status_weather', Status_antenna_msg, st.callback2)
+    sub2 = rospy.Subscriber('status_weather', Weather_msg, st.callback2)
     print(sub2)
     rospy.spin()
